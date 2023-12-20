c4c61f3e2e9d3dee5985c6a0a51c19ee8694a67a
Gabriel Bermes--Poupeau <gabrielbermesp@gmail.com>
2023-12-20 16:57:46 +0100

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+# import
 import numpy as np
 import torch
 import os
@@ -5,49 +6,66 @@ from PIL import Image
 import clip
 import pathlib
 
-img_folder = "static/"
+def InitialiseModel():
 
-print(" reading ", img_folder)
-data_dir = pathlib.Path(img_folder)
-image_count = len(list(data_dir.glob('*.*')))
-print("number of images: ", image_count)
+    # image folder
+    img_folder = "static/"
 
-print("Torch version:", torch.__version__)
+    # read image
+    print(" reading ", img_folder)
+    data_dir = pathlib.Path(img_folder)
+    image_count = len(list(data_dir.glob('*.*')))
+    print("number of images: ", image_count)
 
-clip.available_models()
+    # torch version
+    print("Torch version:", torch.__version__)
 
-model, preprocess = clip.load("ViT-B/32")
-model.eval()
-input_resolution = model.visual.input_resolution
-context_length = model.context_length
-vocab_size = model.vocab_size
+    # list clip model
+    clip.available_models()
 
-print("Model parameters :" f"{np.sum([int(np.prod(p.shape)) for p in model.parameters()]):,}")
-print("Input resolution :", input_resolution)
-print("Context length :", context_length)
-print("Vocab size :", vocab_size)
+    # loading model
+    model, preprocess = clip.load("ViT-B/32")
+    model.eval()
+    input_resolution = model.visual.input_resolution
+    context_length = model.context_length
+    vocab_size = model.vocab_size
 
-preprocess
+    print("Model parameters :" f"{np.sum([int(np.prod(p.shape)) for p in model.parameters()]):,}")
+    print("Input resolution :", input_resolution)
+    print("Context length :", context_length)
+    print("Vocab size :", vocab_size)
 
-clip.tokenize("Hello world!")
+    # image preprocessing
+    preprocess
 
-original_images = []
-images = []
-i = 0
+    # text preprocessing
+    clip.tokenize("Hello world!")
 
-for filename in [filename for filename in os.listdir(data_dir) if filename.endswith(".png") or filename.endswith(".jpg")]:
-    name = os.path.splitext(filename)[0]
-    print(i, " : ", name)
-    image = Image.open(os.path.join(data_dir, filename)).convert("RGB")
+    listdir = os.listdir(img_folder)
 
-    original_images.append(image)
-    images.append(preprocess(image))
-    i += 1
+    if listdir:
+        # setting input image
+        original_images = []
+        images = []
+        i = 0
 
-image_input = torch.tensor(np.stack(images))
+        for filename in [filename for filename in os.listdir(data_dir) if filename.endswith(".png") or filename.endswith(".jpg")]:
+            name = os.path.splitext(filename)[0]
+            print(i, " : ", name)
+            image = Image.open(os.path.join(data_dir, filename)).convert("RGB")
 
-with torch.no_grad():
-    image_features = model.encode_image(image_input).float()
-    image_features /= image_features.norm(dim=-1, keepdim=True)
+            original_images.append(image)
+            images.append(preprocess(image))
+            i += 1
 
-torch.save(image_features, "torch.pt")
+        image_input = torch.tensor(np.stack(images))
+
+        with torch.no_grad():
+            image_features = model.encode_image(image_input).float()
+
+            # calculating cosine similarity
+            image_features /= image_features.norm(dim=-1, keepdim=True)
+
+        torch.save(image_features, "tensor.pt")
+    else:
+        print("no image")
